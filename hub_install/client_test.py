acf0c107ccb9bd0e9d5162ce990a8c99e3d50848
office <office@c.com>
2021-06-27 17:47:38 +0800

    hub_install complete

---
+++ hub_install/client_test.py
@@ -0,0 +1,31 @@
+#coding:utf-8
+'''
+@version: python3.6
+@author: ‘eric‘
+@license: Apache Licence
+@contact: steinven@qq.com
+@software: PyCharm
+@file: client_test.py
+@time: 2021/6/27 17:01
+'''
+import base64
+import json
+import os
+
+import requests
+
+
+def cv2_to_base64(image):
+    return base64.b64encode(image).decode('utf8')
+url = "http://172.16.71.234:8866/predict/chinese_ocr_db_crnn_server"
+test_img_dir = '../test_files'
+headers = {"Content-type": "application/json"}
+for idx, img_file in enumerate(os.listdir(test_img_dir)):
+    with open(os.path.join(test_img_dir, img_file), 'rb') as file:
+        image_data1 = file.read()
+    image = cv2_to_base64(image_data1)
+    data = {'images': [image]}
+    r = requests.post(url=url, headers=headers,data=json.dumps(data))
+    print(r.json())
+
+print("==> total number of test imgs: ", len(os.listdir(test_img_dir)))
